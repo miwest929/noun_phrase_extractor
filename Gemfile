ab259cc787d3d3f83def1a1ed0fe1ee189be880a
Michael West <mwest@mdsol.com>
2015-03-27 12:01:26 -0400

    Initial commit of Noun Phrase Extractor for Tweets

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem 'twitter', '~> 5.9'
+gem 'foreman'
+gem 'byebug'
+
+gem 'stanford-core-nlp', '~> 0.5'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
---
+++ noun_phrase.rb
@@ -0,0 +1,76 @@
+StanfordCoreNLP.jar_path = "./stanford-core-nlp/"
+StanfordCoreNLP.model_path = "./stanford-core-nlp/"
+
+PIPELINE = StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse, :ner, :dcoref)
+
+class BaseExtractor
+  def initialize
+  end
+
+  def extract(token_tags)
+    entities = []
+    current_entity = []
+    token_tags.each do |tag|
+      if yield(tag)
+        current_entity << tag
+      else
+        unless current_entity.empty?
+          entities << current_entity
+          current_entity = []
+        end
+      end
+    end
+
+    entities << current_entity unless current_entity.empty?
+
+    entities.map {|tags| tags.map {|t| t.value}.join(' ')}
+  end
+end
+
+class TimeExtractor < BaseExtractor
+  def extract(token_tags)
+    super(token_tags) do |tag|
+      tag.ne_tag == "TIME"
+    end
+  end
+end
+
+class NounPhraseExtractor < BaseExtractor
+  def extract(token_tags)
+    super(token_tags) do |tag|
+      %w(DT JJ NN NNP CC NNS).include?(tag.pos)
+    end
+  end
+end
+
+class TweetParser
+  TokenTag = Struct.new(:value, :pos, :ne_tag, :iob_tag)
+
+  def self.parse(tweet)
+    sentences = token_tuples(tweet)
+
+    sentences.map do |sentence_tags|
+      {
+        nouns: NounPhraseExtractor.new.extract(sentence_tags),
+        times: TimeExtractor.new.extract(sentence_tags)
+      }
+    end
+  end
+
+private
+  def self.token_tuples(tweet)
+    model = StanfordCoreNLP::Annotation.new(tweet)
+
+    PIPELINE.annotate(model)
+    tuples = []
+    model.get(:sentences).each do |sentence|
+      sentence_tuples = []
+      sentence.get(:tokens).each do |token|
+        sentence_tuples << TokenTag.new(token.get(:value).to_s, token.get(:part_of_speech).to_s, token.get(:named_entity_tag).to_s)
+      end
+      tuples << sentence_tuples
+    end
+
+    tuples
+  end
+end
